6f1e1a4c470582bbfdb71262bc3079bef250225e
yikZero <i@yikzero.com>
2025-03-25 08:17:12 +0800

    feat: 更新Next.js配置，添加Shiki主题支持，优化路由重定向，修改全局样式，更新欢迎文章内容

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import createMDX from '@next/mdx';
+import rehypeShiki from '@shikijs/rehype';
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
@@ -8,13 +9,71 @@ import remarkFrontmatter from 'remark-frontmatter';
 import remarkGfm from 'remark-gfm';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.yikzero.com',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/posts',
+        destination: '/blog',
+        permanent: true,
+      },
+      {
+        source: '/posts/:path*',
+        destination: '/blog/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:locale/posts',
+        destination: '/:locale/blog',
+        permanent: true,
+      },
+      {
+        source: '/:locale/posts/:path*',
+        destination: '/:locale/blog/:path*',
+        permanent: true,
+      },
+      {
+        source: '/drafts',
+        destination: '/works',
+        permanent: true,
+      },
+      {
+        source: '/:locale/drafts',
+        destination: '/:locale/works',
+        permanent: true,
+      },
+    ];
+  },
+  compress: false,
+  output: 'standalone',
   pageExtensions: ['mdx', 'ts', 'tsx'],
 };
 
 const withMDX = createMDX({
   options: {
     remarkPlugins: [remarkFrontmatter, remarkGfm],
-    rehypePlugins: [rehypeUnwrapImages, rehypeSlug, rehypeAutolinkHeadings],
+    rehypePlugins: [
+      rehypeUnwrapImages,
+      rehypeSlug,
+      rehypeAutolinkHeadings,
+      [
+        rehypeShiki,
+        {
+          themes: {
+            light: 'github-light',
+            dark: 'github-dark',
+          },
+          inline: 'tailing-curly-colon',
+        },
+      ],
+    ],
   },
 });
 
