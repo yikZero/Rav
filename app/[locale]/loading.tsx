f06e21e78cab83245933c7c208829a1db7b9c631
yikZero <i@yikzero.com>
2025-05-04 20:00:57 +0800

    fix: update loading component styles for improved layout consistency, adjust experience time format in English and Chinese localization files

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className="mx-auto flex min-h-[calc(100svh-172px)] w-full max-w-240 flex-col items-center justify-center px-6">
       <svg
-        className="mb-48 size-6 animate-spin text-strong"
+        className="size-6 animate-spin text-strong"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
@@ -16,7 +16,6 @@ export default function Loading() {
           strokeWidth="4"
         ></circle>
         <path
-          className="opacity-75"
           fill="currentColor"
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         ></path>
